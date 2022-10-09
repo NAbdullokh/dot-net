267fd76c5f05dd9bcf5a50833e55be9bf962bb32
NAbdullokh <country.abu@gmail.com>
2022-10-09 22:51:45 +0500

    .net

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyFirstProject
+{
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello";
+            Console.WriteLine($"Hello\a + {s}");
+        }
+    } 
+} 
