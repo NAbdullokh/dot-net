28365964b6aa07677596d91506915e98355bf096
NAbdullokh <country.abu@gmail.com>
2022-10-10 22:46:35 +0500

    test

--- Program.cs
+++ Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using static System.Console;
-
-
-namespace MyFirstProject
-{
-internal class Program
-    {
-        static void Main(string[] args)
+﻿    class Program
+    { 
+    static void Main(string[] args)
         {
-        ConsoleKeyInfo info =  Console.ReadKey();
-            WriteLine(info.Key);
+        Console.WriteLine(Add(3,4));
         }
-    } 
-} 
+    public static int Add(int num1, int num2)
+        {
+        return num1 + num2;
+        }
+    }
